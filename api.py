8f6c2a6f179ea3b508f94f34ca1bebcac6e073c2
Pinaki19 <pinakibanerjee056@gmail.com>
2024-03-20 11:37:49 +0530

    Initial commit

---
+++ api.py
@@ -0,0 +1,197 @@
+import asyncio
+import json
+import random
+from fastapi import FastAPI, HTTPException, Request, WebSocket
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import HTMLResponse, FileResponse
+from pydantic import BaseModel
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import JSONResponse
+from fastapi.security import APIKeyCookie
+import uuid
+import os
+import uvicorn
+# Import pymongo
+import pymongo
+from datetime import datetime
+from typing import Union,Dict,List,Set
+
+import string
+
+
+class Move(BaseModel):
+    row:int
+    column:int
+
+class User(BaseModel):
+    name: str
+    profile_pic: str
+
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+app.mount("/public", StaticFiles(directory="public"), name="public")
+templates = Jinja2Templates(directory="public/html")
+
+game_mapping: Dict[str, List[str]] = {}
+cur_games: Dict[str,str] = {}
+acknowledgements:Dict[str,Set[str]] = {}
+connections: Dict[str, Dict[str,WebSocket]] = {}
+
+game_state:Dict[str,List[List[int]]]={}
+
+@app.get("/")
+async def welcome_user(request: Request, user: str = "user"):
+    session_id = request.cookies.get("session_id")
+    if session_id is None:
+        session_id = str(uuid.uuid4())
+    response = FileResponse("public/html/index.html")
+    response.set_cookie(key="session_id", value=session_id)
+    return response
+
+@app.get("/new_game")
+async def new_game():
+    return FileResponse("public\html\hex.html")
+
+
+@app.get("/create-game")
+async def create_game(request: Request) -> Dict[str, str]:
+    """
+    Create a new game with a unique game ID and generate two session IDs.
+    """
+    game_id = str(uuid.uuid4())  # Generate a unique game ID
+    # Generate session ID for player 1
+    session_id1 = request.cookies.get("session_id")
+    if session_id1 is None:
+        session_id1 = str(uuid.uuid4())
+    session_id2 = str(uuid.uuid4())  # Generate session ID for player 2
+    # Store the mapping between game ID and session IDs
+    game_mapping[game_id] = [session_id1,session_id2]
+    cur_games[session_id1]=game_id
+    response = FileResponse("public/html/hex.html")
+    response.set_cookie(key="session_id", value=session_id1)
+    response.set_cookie(key="game_id",value=game_id)
+    return response
+
+
+@app.get("/join-game/{game_id}")
+async def join_game(game_id: str,request: Request) -> str:
+    """
+    Join the game with the provided game ID using the given session ID.
+    """
+    s="  ".join([str(i) for i in game_mapping.keys()])
+    if game_id not in game_mapping:
+        raise HTTPException(status_code=404, detail=s)
+    id2=game_mapping[game_id][1]
+    if id2 in cur_games and cur_games[id2]==game_id:
+        raise HTTPException(
+            status_code=400, detail="Game already has two players")
+    response = FileResponse("public/html/hex.html")
+    response.set_cookie(key="session_id", value=id2)
+    response.set_cookie(key="game_id", value=game_id)
+    cur_games[id2]=game_id
+    return response
+
+
+@app.websocket("/ws/{game_id}/{session_id}")
+async def websocket_endpoint(websocket: WebSocket, game_id: str, session_id: str):
+    await websocket.accept()
+    if game_id not in connections:
+        connections[game_id] = {}
+    try:
+        while True:
+            # Receive message from client (acknowledgment)
+            data = await websocket.receive_text()
+            print(data)
+            if data == '{"action":"acknowledge"}':
+                connections[game_id][session_id]=websocket
+                await handle_acknowledgment(game_id)
+    except Exception as e:
+        print(f"WebSocket connection closed with exception: {e}")
+    
+
+async def handle_acknowledgment(game_id: str):
+    if len(connections.get(game_id, {})) == 2:
+        await start_game(game_id)
+
+
+async def start_game(game_id: str):
+    if (len(connections[game_id].values())!=2):
+        return False
+    temp=[]
+    for i in range(11):
+        temp.append([0 for i in range(11)])
+    game_state[game_id]=temp
+    s1,s2=game_mapping[game_id]
+    sock1,sock2=connections[game_id][s1],connections[game_id][s2]
+    turn = random.randint(0, 1)
+    if(turn==1):
+        sock1, sock2 = connections[game_id][s2], connections[game_id][s1]
+    await sock1.send_text('{"Type":1,"readyToStart": true,"turn":true}')
+    await sock2.send_text('{"Type":1,"readyToStart": true,"turn":false}')
+    
+    
+@app.post("/make_move")
+async def make_move(request: Request, data:Move):
+    # Retrieve game_id and session_id from cookies
+    data=data.dict()
+    row=data.get("row",-1)
+    column=data.get("column",-1)
+    game_id = request.cookies.get("game_id")
+    session_id = request.cookies.get("session_id")
+    player_number = 0 if game_mapping[game_id][0] == session_id else 1
+    for session_id in game_mapping[game_id]:
+        if session_id not in connections[game_id]:
+            return {"message": "waiting for other player"}
+    if not game_id or not session_id:
+        raise HTTPException(
+            status_code=400, detail="Game ID or session ID not found in cookies")
+
+    # Retrieve player number based on session_id
+    
+    if player_number is None:
+        raise HTTPException(
+            status_code=400, detail="Player number not found for session ID")
+
+    # Retrieve game state (matrix) for the given game_id
+    game = game_state.get(game_id)
+    if game is None:
+        raise HTTPException(
+            status_code=404, detail="Game state not found for game ID")
+    # Update the game state with the player's move
+    flag=False
+    if (row < 0 or column < 0):
+        flag=False
+    else:
+        if(game[row][column]!=0):
+            raise HTTPException(
+                status_code=404, detail="Invalid move")
+        game[row][column] = player_number+1
+        flag=True
+    t= 0 if player_number==1 else 1
+    sock1 = connections[game_id][game_mapping[game_id][t]]
+    sock2 = connections[game_id][game_mapping[game_id][(t+1)%2]]
+    
+    message1 = json.dumps({"Type": 2, "turn": True})
+    message2 = json.dumps({"Type": 2, "turn": False})
+    if(flag):
+        message1 = json.dumps(
+            {"Type": 0, "row": row, "column": column, "turn": True})
+        message2 = json.dumps(
+            {"Type": 0, "row": row, "column": column, "turn": False})
+    
+    await sock1.send_text(message1)
+    await sock2.send_text(message2)
+    return {"message": "Move successfully made"}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app,port=9000)
---
+++ public/App.js
@@ -0,0 +1,308 @@
+const url = "public/11_2w4_2000.onnx";
+let sess_init = false;
+let info = "";
+let max_rating;
+const sess = new onnx.InferenceSession();
+
+async function load_model() {
+    await sess.loadModel(url);
+    sess_init = true;
+}
+
+function PosToId(x, y) {
+    return x + board_size * y;
+}
+
+function IdToPos(id) {
+    const x = id % board_size;
+    const y = (id - x) / board_size;
+    return [x, y];
+}
+
+// Assuming boardSize is 11x11 and the board is represented as a 1D array of size 121
+function HasWinner(board) {
+    // Define directions for hexagon grid
+    const directions = [[-1, 0], [-1, 1], [0, -1], [0, 1], [1, -1], [1, 0]];
+
+    // Function to check if a cell is valid
+    const isValidCell = (row, col, check) => {
+        return row >= 0 && row < 11 && col >= 0 && col < 11 && board[col * 11 + row] === check;
+    };
+
+    // Function to perform DFS traversal
+    const dfs = (row, col, visited, targetRow, targetCol, check) => {
+        visited.add(col * 11 + row);
+        //console.log(visited);
+        // If we reach the opposite side, we have a winner
+        if ((targetRow === 10 && row === targetRow) || (targetCol === 10 && col === targetCol)) {
+            winner=check;
+            return true;
+        }
+
+        // Check all neighbors
+        for (const [dx, dy] of directions) {
+            const newRow = row + dx;
+            const newCol = col + dy;
+
+            // If the neighbor is valid and hasn't been visited, explore it
+            if (isValidCell(newRow, newCol, check) && !visited.has(newCol * 11 + newRow)) {
+                if (dfs(newRow, newCol, visited, targetRow, targetCol, check)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    };
+
+    // Check for each starting position on the top and bottom sides
+    for (let row = 0; row < 11; row++) {
+        if (board[row] === "0") {
+            const visited = new Set();
+            if (dfs(row, 0, visited, 11, 10, "0")) {
+                return true;
+            }
+        }
+    }
+    for (let col = 0; col < 121; col += 11) {
+        if (board[col] === "1") {
+            const visited = new Set();
+            if (dfs(0, col / 11, visited, 10, 11, "1")) {
+                return true;
+            }
+        }
+    }
+
+    return false; // No winner found
+}
+
+
+
+
+function minimax(board, depth, current_player, first_player) {
+    const other_player = current_player === "0" ? "1" : "0";
+    if (HasWinner(board)) {
+        console.log("Yes has winner");
+        // other player won.
+        if (current_player === "0") {
+            return [-1, null]; // blue won
+        } else {
+            return [1, null]; // red won
+        }
+    }
+
+    if (depth === 0) {
+        return [0, null]; // no one won
+    }
+    if (current_player === "1") { // red, maximizing
+        let value = -10;
+        let best = null;
+        for (let i = 0; i < board_size * board_size; i++) {
+            if (board[i] !== null) {
+                continue;
+            }
+            board[i] = current_player;
+            const a = minimax(board, depth - 1, other_player, first_player);
+            board[i] = null;
+            if (a[0] > value) {
+                value = a[0];
+                best = i;
+            }
+            if (value >= 1) {
+                // this is good enough
+                return [value, best];
+            }
+            if (current_player !== first_player && value === 0) {
+                // this is good enough for the second player.
+                return [value, best];
+            }
+        }
+        return [value, best];
+    } else { // blue, minimizing
+        let value = 10;
+        let best = null;
+        for (let i = 0; i < board_size * board_size; i++) {
+            if (board[i] !== null) {
+                continue;
+            }
+            board[i] = current_player;
+            const a = minimax(board, depth - 1, other_player, first_player);
+            board[i] = null;
+            if (a[0] < value) {
+                value = a[0];
+                best = i;
+                if (value <= -1) {
+                    // this is bad enough
+                    return [value, best];
+                }
+                if (current_player !== first_player && value === 0) {
+                    // this is good enough for the second player.
+                    return [value, best];
+                }
+            }
+        }
+        return [value, best];
+    }
+}
+
+function AddBorder(x, y, input_values, border_color) {
+    if ([-1, board_size].includes(x) && [-1, board_size].includes(y)) {
+        input_values.push(0);
+        return true;
+    }
+    if (!([-1, board_size].includes(x) || [-1, board_size].includes(y))) {
+        return false;
+    }
+    if ([-1, board_size].includes(x)) {
+        input_values.push(border_color ? 1 : 0);
+    } else {
+        input_values.push(border_color ? 0 : 1);
+    }
+    return true;
+}
+
+function findSureWinMove(board, player) {
+    for (let depth of [1, 4]) {
+        const a = minimax(board, depth, player, player);
+        if (player === '1') {
+            if (a[0] > 0) {
+                return a[1];
+            }
+        } else {
+            if (a[0] < 0) {
+                return a[1];
+            }
+        }
+    }
+
+    return null;
+}
+
+async function handle_click(index, matrix, player, agent, ai_board,agent_is_blue){
+    return new Promise((resolve, reject) => {
+        const sure_win_move = findSureWinMove(ai_board, agent);
+        if (sure_win_move !== null) {
+            matrix[index] = agent;
+            set_color(index, true);
+            resolve(index);
+            return;
+        }
+        runModel(ai_board,agent_is_blue).then((result) => {
+            let best = -1;
+            for (let i = 0; i < board_size * board_size; i++) {
+                if (matrix[i] === null) {
+                    if (best === -1 || result[i] > max_rating) {
+                        best = i;
+                        max_rating = result[i];
+                    }
+                }
+            }
+            let score_sum = 0;
+            for (let i = 0; i < board_size * board_size; i++) {
+                if (matrix[i] === null) {
+                    const score = Math.pow(2, result[i] - max_rating);
+                    score_sum += score;
+                }
+            }
+            let id = index;
+
+            let test_board = Array.from(matrix);
+            test_board[id] = player;
+            test_board[best] = agent;
+            const sure_win = findSureWinMove(test_board, player);
+            if (sure_win !== null) {
+                console.log("Player can surely win with the suggested move", best, "Moving to the sure win move instead.");
+                best = sure_win;
+            }
+            resolve(best);
+        }).catch(reject);
+    });
+}
+
+async function runModel(cells,agent_is_blue) {
+    return new Promise(async (resolve, reject) => {
+        try {
+            info = "waiting for agent to move...";
+            if (!sess_init) {
+                await load_model();
+            }
+
+            let input_values = [];
+            if (agent_is_blue) {
+                for (let x = -1; x < board_size + 1; x++) {
+                    for (let y = -1; y < board_size + 1; y++) {
+                        if (!AddBorder(x, y, input_values, 1)) {
+                            const id = PosToId(x, y);
+                            input_values.push(cells[id] === "1" ? 1 : 0);
+                        }
+                    }
+                }
+                for (let x = -1; x < board_size + 1; x++) {
+                    for (let y = -1; y < board_size + 1; y++) {
+                        if (!AddBorder(x, y, input_values, 0)) {
+                            const id = PosToId(x, y);
+                            input_values.push(cells[id] === "0" ? 1 : 0);
+                        }
+                    }
+                }
+            } else {
+                for (let y = -1; y < board_size + 1; y++) {
+                    for (let x = -1; x < board_size + 1; x++) {
+                        if (!AddBorder(x, y, input_values, 0)) {
+                            const id = PosToId(x, y);
+                            input_values.push(cells[id] === "0" ? 1 : 0);
+                        }
+                    }
+                }
+                for (let y = -1; y < board_size + 1; y++) {
+                    for (let x = -1; x < board_size + 1; x++) {
+                        if (!AddBorder(x, y, input_values, 1)) {
+                            const id = PosToId(x, y);
+                            input_values.push(cells[id] === "1" ? 1 : 0);
+                        }
+                    }
+                }
+            }
+
+            let input_values2 = [];
+            for (let id = 0; id < (board_size + 2) * (board_size + 2); id++) {
+                input_values2.push(input_values[(board_size + 2) * (board_size + 2) - id - 1]);
+            }
+            for (let id = 0; id < (board_size + 2) * (board_size + 2); id++) {
+                input_values2.push(input_values[2 * (board_size + 2) * (board_size + 2) - id - 1]);
+            }
+
+            const outputTensor = await evalModel(input_values);
+            const outputTensor2 = await evalModel(input_values2);
+    
+            let average_output = [];
+            for (let id = 0; id < board_size * board_size; id++) {
+                average_output.push((outputTensor[id] + outputTensor2[board_size * board_size - id - 1]) / 2);
+            }
+            let final_output = [];
+            if (agent_is_blue) {
+                // need to transpose
+                for (let x = 0; x < board_size; x++) {
+                    for (let y = 0; y < board_size; y++) {
+                        const id = PosToId(x, y);
+                        final_output.push(average_output[id]);
+                    }
+                }
+            } else {
+                final_output = average_output;
+            }
+        
+            resolve(final_output);
+        } catch (e) {
+            reject(e);
+        }
+    });
+}
+
+
+async function evalModel(input_array) {
+    const inputTensor = new onnx.Tensor(new Float32Array(input_array), 'float32', [1, 2, board_size + 2, board_size + 2]);
+    const outputMap = await sess.run([inputTensor]);
+    const outputTensor = outputMap.values().next().value;
+    const outputData = outputTensor.data;
+    return outputData;
+}
